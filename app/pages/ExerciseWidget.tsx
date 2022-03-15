6e03471895541bb7a3a0afeeea54a20fdc2dcd8f
Joseph Wood <40001404+JosephWood2001@users.noreply.github.com>
2022-03-14 20:38:01 -0400

    exercisewidget displays information

--- app/pages/ExerciseWidget.tsx
+++ app/pages/ExerciseWidget.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Card, Title, Button, Paragraph } from 'react-native-paper';
+import { Card, Title, Button, Paragraph, Text } from 'react-native-paper';
 import Exercise from '../objects/Exercise';
 
 
 function ExerciseWidget(props : {exercise:Exercise}) {
     let exercise = props.exercise as Exercise;
+    let attributes = [];
+    //adds all the attributes of exercise to attributes array
+    if(exercise.duration != null){
+        attributes.push(["Duration",exercise.duration]);
+    }
+    if(exercise.weight != null){
+        attributes.push(["Weight",exercise.weight]);
+    }
+    if(exercise.reps != null){
+        attributes.push(["Reps",exercise.reps]);
+    }
+    if(exercise.sets != null){
+        attributes.push(["Sets",exercise.sets]);
+    }
+
     return (
         <Card>
             <Card.Title
                 title= {exercise.exerciseName}
             />
             <Card.Content>
-                <Paragraph>TODO: make this a list of the values that arn't null</Paragraph>
+                {
+                    attributes.map((values) => (
+                        <Text style={{fontWeight:'bold'}}>{values[0]}<Text style={{fontWeight:'normal'}}> {values[1]}</Text></Text>
+                    ))
+                }
             </Card.Content>
             <Card.Actions>
                 <Button>Edit</Button>
